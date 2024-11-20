0fd5490f262f0f4bb06a9e025354ef7e3737829d
acid <vitiazagortsev@gmail.com>
2024-11-20 18:33:26 +0300

    work on ui layer need

--- WPFUI/ViewModels/MainViewModel.cs
+++ WPFUI/ViewModels/MainViewModel.cs
@@ -35,6 +35,7 @@ namespace WPFUI.ViewModels
             if (SelectedEntity == "Билеты")
             {
                 //CurrentView = new TicketViewModel();
+                //
             }
         }
 
